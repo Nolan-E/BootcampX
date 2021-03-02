26c927364b32ec2a633212cda6d4e76b05514200
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-02 16:09:01 -0700

    Add assistance requests table to database.

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -6,3 +6,14 @@ CREATE TABLE teachers (
   end_date DATE
 );
 
+CREATE TABLE assistance_requests (
+  id SERIAL PRIMARY KEY NOT NULL,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  created_at TIMESTAMP,
+  started_at TIMESTAMP,
+  completed_at TIMESTAMP,
+  student_feedback TEXT,
+  teacher_feedback TEXT
+);
