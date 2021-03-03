f2f0982201c2e06cf1425fe924dffe8c2edbc7f2
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-02 18:13:37 -0700

    Add total assignments and duration query. Update overwritten code in most confusing assignments query.

--- 4_queries/10_most_confusing_assignments.sql
+++ 4_queries/10_most_confusing_assignments.sql
@@ -1,4 +1,4 @@
-SELECT assignments.id, name, day, chapter, count(assistance_requests.*) AS total_requests
+SELECT assignments.id, assignments.name, assignments.day, assignments.chapter, count(assistance_requests.*) AS total_requests
 FROM assignments
 JOIN assistance_requests ON assignments.id = assignment_id
 GROUP BY assignments.id
