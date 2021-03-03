39b1f2cab88e52ea9c8206cd5c6b086897b90dcc
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-02 17:09:58 -0700

    Fix overwritten average assistance time query. Add average cohort assistance time query.

--- 4_queries/4_average_assistance_time.sql
+++ 4_queries/4_average_assistance_time.sql
@@ -1,6 +1,2 @@
-SELECT cohorts.name, avg(completed_at - started_at) AS average_assistance_time
-FROM assistance_requests
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-GROUP BY cohorts.name
-ORDER BY average_assistance_time;
+SELECT avg(completed_at - started_at) AS average_assistance_request_duration
+FROM assistance_requests;
